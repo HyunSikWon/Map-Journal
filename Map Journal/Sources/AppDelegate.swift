ce846e058ec5aee361c7a07cc41ce37baf43eb3c
hyunsik won <shik0601@gmail.com>
2021-04-09 15:35:36 +0900

    프로젝트 초기 세팅

---
+++ Map Journal/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Map Journal
+//
+//  Created by 원현식 on 2021/04/09.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    self.window = UIWindow()
+    window?.rootViewController = MapViewController()
+    window?.makeKeyAndVisible()
+    return true
+  }
+}
+
---
+++ Map Journal/Sources/Presentation/Main/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Map Journal
+//
+//  Created by 원현식 on 2021/04/09.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
