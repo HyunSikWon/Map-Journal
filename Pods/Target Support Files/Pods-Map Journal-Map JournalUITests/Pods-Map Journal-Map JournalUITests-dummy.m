0dada9a6a122b0e163f21f0dfff969868aa18039
hyunsik won <shik0601@gmail.com>
2021-04-13 21:47:40 +0900

    chore: pod 추가

---
+++ Pods/Target Support Files/Pods-Map Journal-Map JournalUITests/Pods-Map Journal-Map JournalUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Map_Journal_Map_JournalUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Map_Journal_Map_JournalUITests
+@end
---
+++ Pods/Target Support Files/Pods-Map JournalTests/Pods-Map JournalTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Map_JournalTests : NSObject
+@end
+@implementation PodsDummy_Pods_Map_JournalTests
+@end
